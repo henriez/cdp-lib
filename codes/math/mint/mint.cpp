c4b92bd6736e923f39f1e074137502568c7b1d0b
Henrique Mendes <hapmendesss@gmail.com>
2026-06-10 17:20:00 -0300

    mint implementation now safe for MOD >= INT_MAX/2

--- codes/math/mint/mint.cpp
+++ codes/math/mint/mint.cpp
@@ -1,41 +1,42 @@
-template<ll MOD>
+template<const int MOD>
 struct mint {
-  ll val;
-  mint(ll v = 0) { 
-    if (v < 0) v = v % MOD + MOD; 
-    if (v >= MOD) v %= MOD; 
-    val = v; 
+  int val;
+  mint(ll v=0){
+    ll x = v%MOD;
+    if (x<0) x+=MOD;
+    val = x;
   }
-  mint& operator+=(const mint& other) { 
-    val += other.val; 
-    if (val >= MOD) val -= MOD; 
-    return *this; 
+  mint& operator+=(const mint &o){
+    ll x = (ll)val + o.val;
+    if (x >= MOD) x -= MOD;
+    val = x;
+    return *this;
   }
-  mint& operator-=(const mint& other) { 
-    val -= other.val; 
-    if (val < 0) val += MOD; 
-    return *this; 
+  mint& operator-=(const mint &o){
+    ll x = (ll)val - o.val;
+    if (x < 0) x += MOD;
+    val = x;
+    return *this;
   }
-  mint& operator*=(const mint& other) { 
-    val = (val * other.val) % MOD; 
-    return *this; 
+  mint& operator*=(const mint &o){
+    val = (1ll*val*o.val)%MOD;
+    return *this;
   }
-  mint& operator/=(const mint& other) { 
-    val = (val * inv(other).val) % MOD; 
-    return *this; 
+  mint& operator/=(const mint &o){
+    val = (1ll*val*inv(o).val)%MOD;
+    return *this;
   }
-  friend mint operator+(mint a, const mint& b) { return a += b; }
-  friend mint operator-(mint a, const mint& b) { return a -= b; }
-  friend mint operator*(mint a, const mint& b) { return a *= b; }
-  friend mint operator/(mint a, const mint& b) { return a /= b; }
-  static mint power(mint b, ll e) { 
-    mint ans = 1; 
-    while (e > 0) { 
-      if (e & 1) ans *= b; 
-      b *= b; 
-      e /= 2; 
-    } 
-    return ans; 
+  friend mint operator+(mint a, const mint &b) { return a+=b; }
+  friend mint operator-(mint a, const mint &b) { return a-=b; }
+  friend mint operator*(mint a, const mint &b) { return a*=b; }
+  friend mint operator/(mint a, const mint &b) { return a/=b; }
+  static mint power(mint b, int e){
+    mint ans = 1;
+    while(e>0){
+      if (e&1) ans *= b;
+      b*=b; e>>=1;
+    }
+    return ans;
   }
-  static mint inv(mint n) { return power(n, MOD - 2); }
+  static mint inv(mint n){ return power(n,MOD-2); }
 };
